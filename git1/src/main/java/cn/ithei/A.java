9a413c922965b640b8ec106ddc649906d25dea7f
git <git>
2019-06-08 20:27:56 +0800

    sava

---
+++ git1/src/main/java/cn/ithei/A.java
@@ -0,0 +1,13 @@
+package cn.ithei;
+
+/**
+ * Demo class
+ *
+ * @author ZhengXin
+ * @date 2019/6/8
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println(12212);
+    }
+}
